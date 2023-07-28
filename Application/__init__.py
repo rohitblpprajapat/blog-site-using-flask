43c4c1695f2881295240487fd0c08536a9823140
blprohit <rohitblpprajapat@gmail.com>
2023-07-28 12:03:38 +0530

    login & register functionality

--- Application/__init__.py
+++ Application/__init__.py
@@ -1,17 +1,17 @@
 import os
 from flask import Flask
 
-def create_app(test_config = None):
-    #creating and configuring app
+def create_app(test_config=None):
+    # creating and configuring app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY = 'dev',
-        DATABASE = os.path.join(app.instance_path, 'Application.sqlite'),
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'Application.sqlite'),
     )
     
     if test_config is None:
-        # Load the instance congig, if it exists, when not testing
-        app.config.from_pyfile('congig.py', silent=True)
+        # Load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
@@ -20,16 +20,21 @@ def create_app(test_config = None):
     try:
         os.makedirs(app.instance_path)
     except OSError:
-        return 'not found'
         pass
     
+    # a simple page that says hello
     @app.route('/')
-    def home():
+    def hello():
         return 'Jai Siya Ram'
     
-    # from . import db
-    # db.init_app(app)
+    #database thing.......
+    from . import db
+    db.init_app(app)
     
     
-    return app
+    #blueprints thing
+    from . import auth
+    app.register_blueprint(auth.bp)
+    
 
+    return app
